04be79b2640a7d25ade9d9260d54adcc858eee5a
titaniumbutter <israelfranco1998@gmail.com>
2021-06-25 11:22:57 -0500

    inital commit

---
+++ tictactoe.test.ts
@@ -0,0 +1,20 @@
+import { Builder, Capabilities, By } from "selenium-webdriver"
+
+const chromedriver = require('chromedriver')
+
+const driver = new Builder().withCapabilities(Capabilities.chrome()).build()
+
+beforeAll(async () => {
+    await driver.get('http://localhost:4000')
+})
+
+afterAll(async () => {
+    await driver.quit()
+})
+
+test('I can start a game', async () => {
+
+    let button = await (await driver).findElement(By.id('start-game'));
+    await button.click();
+    
+});
